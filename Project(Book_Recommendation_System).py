6ec8e0af88b48bd37648de667e14c47be68d4dab
Marpaka Pradeep Sai <153713502+MarpakaPradeepSai@users.noreply.github.com>
2024-11-27 13:37:37 +0530

    Update Project(Book_Recommendation_System).py

--- Project(Book_Recommendation_System).py
+++ Project(Book_Recommendation_System).py
@@ -10,7 +10,7 @@ final_filtered_df = pd.read_csv('final_filtered_df.csv')
 book_urls_df = pd.read_csv("Books.csv")
 book_urls_df.rename(columns={'Book-Title': 'title'}, inplace=True)
 
-final_filtered_df = final_filtered_df.set_index('title').join(book_urls_df.set_index('title'), how='left').reset_index()
+final_filtered_df = pd.merge(final_filtered_df, book_urls_df, on='title', how='left')
 
 # URL to replace
 url1 = 'http://images.amazon.com/images/P/0690040784.01.LZZZZZZZ.jpg'
