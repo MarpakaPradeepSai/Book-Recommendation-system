9b852762acd4595ab3c8e4bb46326154240dde59
Marpaka Pradeep Sai <153713502+MarpakaPradeepSai@users.noreply.github.com>
2025-03-29 19:08:12 +0530

    Update Project(Book_Recommendation_System).py

--- Project(Book_Recommendation_System).py
+++ Project(Book_Recommendation_System).py
@@ -47,6 +47,10 @@ def get_top_similar_books(book_title, n=10):
     similar_books = similar_scores.sort_values(ascending=False)[1:n+1]
     return similar_books
 
+# Function to get book suggestions based on user input
+def get_book_suggestions(input_text):
+    return final_filtered_df[final_filtered_df['title'].str.contains(input_text, case=False, na=False)]['title'].unique().tolist()
+
 # Streamlit app
 st.title('Book Recommendation System')
 
@@ -129,14 +133,12 @@ st.markdown("""
 st.markdown("<p class='subheader'>Let Us Help You Choose Your Next Book!</p>", unsafe_allow_html=True)
 st.image('https://img.freepik.com/premium-vector/bookcase-with-books_182089-197.jpg', use_container_width=True)
 
-# Create a selectbox for book title with autocomplete (outside of button logic)
+# Create a selectbox for book title with autocomplete
 all_books = final_filtered_df['title'].unique().tolist()
 book_title = st.selectbox('Enter a book title:', [''] + all_books, key='book_title')
 
-# Create the number input for recommendations (outside of button logic)
 num_recommendations = st.number_input('Enter the number of recommendations:', min_value=1, max_value=50, value=10)
 
-# Trigger logic only when the button is pressed
 if st.button('Recommend books'):
     if book_title:
         if book_title != '':
@@ -146,7 +148,7 @@ if st.button('Recommend books'):
             else:
                 st.markdown(f"<div style='font-size:15px;'>Top {num_recommendations} recommendations for '<strong>{book_title}</strong>':</div>", unsafe_allow_html=True)
                 st.write("")
-
+                
                 # Display books in rows with images, horizontal and vertical lines
                 for i in range(0, len(similar_books), 3):
                     cols = st.columns(3)
@@ -174,7 +176,7 @@ if st.button('Recommend books'):
                 # Add extra space between books and final image
                 st.markdown("<div class='extra-space'></div>", unsafe_allow_html=True)
                 st.markdown("<div class='extra-space'></div>", unsafe_allow_html=True)
-
+                
                 # Display the final images
                 st.image('https://theweekjunior.co.uk/sites/default/files/inline-images/DM-596_TWJ_Landing_page_text_02.png', use_container_width=True)
                 st.image('https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHV3M2w0bW9mMnNzeG05NTBzcjFzc29uY21yeWw1aXhnZm40bzlleSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/wIVA0zh5pt0G5YtcAL/giphy.webp', use_container_width=True)
